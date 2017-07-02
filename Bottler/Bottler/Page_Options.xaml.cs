eb73649c1e27e5dd83c5736aabd2920f24502d27
Niklas <niklas.freybier@outlook.de>
2017-07-02 13:10:19 +0200

    Projektdateien hinzufügen.

---
+++ Bottler/Bottler/Page_Options.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Bottler
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page_Options : ContentPage
+    {
+        public Page_Options(string param)
+        {
+            InitializeComponent();
+            push1.Text = param;
+        }
+
+        private async void Button_back_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}

d1e1ad87d1d59490e946213646c0716cda1163a8
Niklas <trash@phoenixz.de>
2017-07-09 22:11:49 +0200

    .

---
+++ Bottler/Bottler/Pages/Stocktaking.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using Emgu.CV.CvEnum;
+
+namespace Bottler.Pages
+{
+    public partial class Stocktaking : ContentPage
+    {
+        //private Capture _caputure = null;
+        public Stocktaking()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
